75c3872e962f3f7304111097b6313c8c3ddd9376
Maurice Schoenmakers <m@x-x.io>
2014-03-31 00:31:52 +0200

    added sha-bang

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 cat $@ | node $DIR/../node_modules/dox-basic/bin/dox-basic -m -t "[![Build Status](https://travis-ci.org/x-component/$npm_package_name.png?v$npm_package_version)](https://travis-ci.org/x-component/$npm_package_name)" > README.md
 node $DIR/../node_modules/projectz/bin/projectz compile
